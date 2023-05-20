f60053cf90ca4e68c6c70c8c860b309c648fbc3e
Afeez Lawal <afeezlg@gmail.com>
2023-05-20 22:01:19 +0100

    chore: add restaurant schema

---
+++ src/restaurant/schemas/restaurant.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type RestaurantDocument = HydratedDocument<Restaurant>;
+
+@Schema()
+export class Restaurant {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  address: string;
+}
+
+export const RestaurantSchema = SchemaFactory.createForClass(Restaurant);

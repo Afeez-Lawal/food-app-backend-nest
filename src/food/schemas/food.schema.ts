6094ba59b03d4dfc1c55b42e3f1ad3ecc08d57f6
Afeez Lawal <afeezlg@gmail.com>
2023-06-05 11:20:15 +0100

    feat: add food resource

---
+++ src/food/schemas/food.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Restaurant } from './../../restaurant/schemas/restaurant.schema';
+
+export type FoodDocument = HydratedDocument<Food>;
+
+@Schema()
+export class Food {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  price: string;
+
+  @Prop({ required: true })
+  image: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' }] })
+  restaurant: Restaurant;
+}
+
+export const FoodSchema = SchemaFactory.createForClass(Food);
